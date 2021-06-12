e07f8913dad03f9819564f19dbd0e85b819fbe3e
cfinkenbiner <c.finkenbiner@gmail.com>
2021-06-12 10:52:03 -0400

    draft of ds script + output csv

--- main_iso_precip.py
+++ main_iso_precip.py
@@ -15,17 +15,45 @@ Created on Fri Jun 11 18:08:26 2021
 '''
 
 import precip_iso_downscaling_scripts as iso_ds
+import pandas as pd
+import numpy as np
 
-### location of data
+### Create dataframe to write to and decide where to save it
+df_d2H = pd.DataFrame(index=pd.to_datetime([])) # empty to begin
+df_d18O = pd.DataFrame(index=pd.to_datetime([])) # empty to begin
+out_dir = '/Users/catiefinkenbiner/Documents/NEON/NEON_daily_isotope_dataset/NEON_daily_iso/'
+
+
+### location of NOEN 30 min Precip + Isotope Data
 file_dir = '/Users/catiefinkenbiner/Documents/NEON/Downscaled_Product/Temporal_Downscaling_NEON_Pisotopes/DATA/'
 
+
 ### site list
-site_list = 'ONAQ'
+site_list = ["DELA", "LENO", "TALL", "BARR", "BONA", "HEAL", "TOOL", "SRER","SYCA", "SJER", "ARIK", "CPER", "NIWO", "RMNP",
+             "STER", "OSBS", "JERC", "PUUM", "KONZ", "UKFS", "SERC", "HARV", "UNDE", "BART", "NOGP", "WOOD", "BLUE", "OAES", "CUPE",
+              "GUAN", "GUIL", "GRSM", "ORNL", "CLBJ", "PRIN", "MOAB", "ONAQ", "REDB", "BLAN", "MLBS", "SCBI", "WREF", "STEI", "YELL"]
 
-### location of 30 min Precip data
-precip_30min_loc = file_dir+'PrecipData/'+str(site_list)+'PrecipData.xlsx'
-iso_p_loc = file_dir+'IsoData/'+str(site_list)+'IsoData.xlsx'
+for s in np.arange(len(site_list)):
+    
+    precip_30min_loc = file_dir+'PrecipData/'+str(site_list[s])+'PrecipData.xlsx'
+    iso_p_loc = file_dir+'IsoData/'+str(site_list[s])+'IsoData.xlsx'
+    
+    precip_filter = 0.25 ## remove small P amts --> edit this if you desire
+    
+    
+    ### runs downscaling method
+    site_name = site_list[0]
+    site_dailyP = iso_ds.read_p_data(site_list[s], precip_30min_loc, precip_filter, iso_p_loc, out_dir)
+    
+    if 'd2H' in site_dailyP.columns:
+        
+        df_d2H[site_list[s]] = site_dailyP['d2H']
+        df_d18O[site_list[s]] = site_dailyP['d18O']
+    else:
+ 
+        df_d2H[site_list[s]] = np.nan
+        df_d18O[site_list[s]] = np.nan
 
-precip_filter = 0.05 ## remove small P amts
 
-iso_ds.read_p_data(precip_30min_loc, precip_filter, iso_p_loc)
+df_d2H.to_csv(out_dir+'daily_d2H.csv')
+df_d18O.to_csv(out_dir+'daily_d18O.csv')
--- precip_iso_downscaling_scripts.py
+++ precip_iso_downscaling_scripts.py
@@ -9,9 +9,12 @@ Created on Fri Jun 11 18:24:31 2021
 import numpy as np
 import pandas as pd
 from datetime import datetime
+import scipy as sp
+from scipy import stats
+from scipy import optimize
 
 
-''' Read time and calculate fractional days '''
+''' Read in date time and calculate fractional days for P and iso data'''
 def changeTimes_change_Pdata(siteDF):
     timeStrs = siteDF['endDateTime']
     newTimes = [] ; fracyr = []
@@ -66,9 +69,205 @@ def getRunningMean(daylist,tsY,tsP,year_frac,w):
     
     return tsYm,tsPm,tsXm,dl
 
+''' Calculate statistics '''
+def calcSiteStats(site_stats, site, lamda, agglev, P, H, O):
+    Pmu = np.mean(P)            # means
+    Hmu = np.mean(H)
+    Omu = np.mean(O)
+    
+    Psigma = np.std(P)           # stds/sigmas
+    Hsigma = np.std(H)
+    Osigma = np.std(O)
+    
+    PH_pearson = sp.stats.pearsonr(P,H)[0]     # rhos
+    PO_pearson = sp.stats.pearsonr(P,O)[0]  
+    HO_pearson = sp.stats.pearsonr(H,O)[0] 
+    
+    site_stats.append([site, agglev, lamda, Pmu, Hmu, Omu,
+                       Psigma, Hsigma, Osigma,
+                       PH_pearson, PO_pearson, HO_pearson])
+    
 
-''' Read precipitation and isotope data '''
-def read_p_data(precip_data_loc, precip_filter, iso_data_loc):
+''' Calculate sinisoidal function + parameters '''
+def sine_func(t, amplitude, phase, offset):
+    return amplitude * np.sin(2*np.pi*t - phase) + offset  # assume phase is decimal year/year
+
+    
+def sine_params(name, agglevel, frac_yr, P, H, O):  
+    param_bounds=([0, -np.pi, -np.inf], [np.inf, np.pi, np.inf])               
+    
+    # d2H - find parameters for sine function based on frac year 
+    params1, params_covariance = optimize.curve_fit(sine_func, frac_yr, H, p0 = [np.std(H)*(2*np.sqrt(2)), np.pi/2,np.mean(H)], bounds = param_bounds)
+    
+    # d18O - find parameters for sine function based on frac year 
+    params2, params_covariance = optimize.curve_fit(sine_func, frac_yr, O, p0 = [np.std(O)*(2*np.sqrt(2)), np.pi/2,np.mean(O)], bounds = param_bounds)
+      
+    return params1, params2
+
+
+''' Use prediction to create conditional copula generated values '''
+def conditional_copula_ts(tsP, xday_stats, H_scale, O_scale):
+    # Observed P statistics
+    Pmu = np.mean(tsP)
+    Psig = np.std(tsP)
+    
+    # Correlation Coefficients
+    xday_rho1 = xday_stats[2,0] ; xday_rho2 = xday_stats[2,1] ; xday_rho3 = xday_stats[2,2] 
+    
+    # Calculate Sigma_bar
+    sigma_bar = [[1 - xday_rho1*xday_rho1, xday_rho3 - xday_rho1*xday_rho2],
+                 [xday_rho3 - xday_rho1*xday_rho2, 1 - xday_rho2*xday_rho2]]
+    
+    series = []
+    for i in np.arange(len(tsP)):
+        if tsP[i] > 0:
+            a = (tsP[i] - Pmu) / Psig        
+            H2mu_bar = xday_rho1 * a
+            O18mu_bar = xday_rho2 * a
+            mu_bar = np.array([H2mu_bar,O18mu_bar])
+
+            X = sp.stats.multivariate_normal.rvs(mean= mu_bar,cov= sigma_bar) 
+            X2 = sp.stats.norm.cdf(X[0], loc=0, scale=1) 
+            X3 = sp.stats.norm.cdf(X[1], loc=0, scale=1)
+
+            index = int(np.floor(X2 * len(H_scale)))
+            newH = H_scale[index]
+
+            index = int(np.floor(X3 * len(O_scale)))
+            newO = O_scale[index]
+
+            series.append([newH, newO]) 
+        else:
+            series.append([np.nan, np.nan])
+    return series
+
+
+def downscaling_to_daily(sitename, daily_P, df_iso, out_dir_loc):
+    print('Downscaling '+ sitename +' ...')
+    
+    '''
+    Step 1 - Remove seasonal time series component (Section 2.b.1 in text)
+    '''
+    
+    # Define Sinisoidal Functions describing seasonality
+    df_iso = df_iso.sort_values('DateTime')
+    df_iso = df_iso.dropna(subset=['Total P'])
+    tsX = df_iso['FracYear'].values 
+    tsP = df_iso['Total P'].values
+    tsO = df_iso['d18OWater'].values
+    tsH = df_iso['d2HWater'].values   
+
+    dayslist = []
+    for dt in np.arange(len(df_iso['DateTime'])):
+        dayslist.append((df_iso['DateTime'].iloc[dt] - df_iso['DateTime'].iloc[0]).days)
+    dayslist = np.array(dayslist)
+
+    # lambda = precipitation frequency (see Eq. 4)
+    p_events = df_iso[df_iso['Total P'].notna()]
+    lamda = len(p_events['Total P'])/((daily_P.index.max() - daily_P.index.min()).days) 
+
+    params1, params2 = sine_params(sitename, 14, tsX, tsP, tsH, tsO) # 14 = biweekly sample, sample frequency
+    tsY_sine_wave = sine_func(tsX, params1[0], params1[1], params1[2]) # solve for amplitude, phase, offset
+    adj_2H = np.array((tsH - tsY_sine_wave))    # remove seasonality from time series 
+
+    tsY_sine_wave = sine_func(tsX, params2[0], params2[1], params2[2])     
+    adj_18O = np.array((tsO - tsY_sine_wave)) 
+    
+    # Get biweekly site stats of stochastic component
+    biweekly_stats = np.array([[np.mean(tsP), np.mean(adj_2H), np.mean(adj_18O)],
+                     [np.std(tsP), np.std(adj_2H), np.std(adj_18O)],
+                     [sp.stats.pearsonr(tsP,adj_2H)[0], sp.stats.pearsonr(tsP,adj_18O)[0], sp.stats.pearsonr(adj_2H,adj_18O)[0]]])
+    
+    '''
+    Step 2 - Predict daily statistics from biweekly time series (Section 2.b.2 in text) Now we will need to aggregate the 
+    stochastic biweekly time series - i.e. calculated weighed running means at biweekly (14-day), 28-day, 42-day, 
+    56-day and 84-day intervals    
+    '''
+
+    # Define time series statistics 
+    site_stats = [[sitename, 14, lamda,
+                   biweekly_stats[0,0], biweekly_stats[0,1], biweekly_stats[0,2],
+                   biweekly_stats[1,0], biweekly_stats[1,1], biweekly_stats[1,2],
+                   biweekly_stats[2,0], biweekly_stats[2,1], biweekly_stats[2,2]]]
+    site_stats_check = len(site_stats)
+
+    for n in np.arange(28,85,14):
+        xday_Hb, xday_Pb, xday_Xb, days = getRunningMean(np.array(dayslist), np.array(tsH), np.array(tsP), tsX, n)
+        xday_Ob, xday_Pb, xday_Xb, days = getRunningMean(np.array(dayslist), np.array(tsO), np.array(tsP), tsX, n)
+        
+        if len(xday_Xb) > 4:
+
+            xday_Xb = np.array(xday_Xb)
+            xday_Pb = np.array(xday_Pb)
+            xday_Hb = np.array(xday_Hb)
+            xday_Ob = np.array(xday_Ob)
+
+            params1a, params2a = sine_params(sitename, n, xday_Xb, xday_Pb, xday_Hb, xday_Ob)
+
+            tsY_sine_wave = sine_func(xday_Xb, params1a[0],params1a[1],params1a[2])
+            adj_2Hb = np.array((xday_Hb - tsY_sine_wave))                           
+
+            tsY_sine_wave = sine_func(xday_Xb, params2a[0],params2a[1],params2a[2])     
+            adj_18Ob = np.array((xday_Ob - tsY_sine_wave))
+        
+            if len(xday_Pb)>2 and len(adj_2Hb)>2 and len(adj_18Ob)>2:
+                calcSiteStats(site_stats, sitename, lamda, n, xday_Pb, adj_2Hb, adj_18Ob)
+            
+    if len(site_stats) == site_stats_check:
+        print("Site {} does not contain sufficient data.".format(sitename))
+        
+    else:        
+        ### the stats are labeled with 'B' here because they are of the stochastic component - not the original time series
+        Site_Stats = pd.DataFrame(site_stats, columns = ['site','agglev','lambda','PmuB','HmuB','OmuB','PsigB',
+                                                         'HsigB','OsigB','PHpB','POpB','HOpB'])
+    
+        ### Now we apply Eq. 4 from Finkenbiner et al. 2021
+        xaxis = np.array(Site_Stats['agglev'].values)
+        yaxis1 = np.array(Site_Stats['HsigB'].values)
+        yaxis2 = np.array(Site_Stats['OsigB'].values)
+
+        def eq4(x, a, b):
+            return b/(x*lamda)**a
+
+        bounds = [[0.2,yaxis1[0]], [0.5,np.inf]]
+        p1, p2 = optimize.curve_fit(eq4, xaxis, yaxis1, p0 = [0.3, yaxis1[0]], bounds=bounds)
+        Hi = float(p1[1]) # estimated daily parameter
+
+        bounds = [[0.2,yaxis2[0]], [0.5,np.inf]]
+        p1, p2 = optimize.curve_fit(eq4, xaxis, yaxis2,  p0 = [0.3, yaxis2[0]], bounds=bounds)
+        Oi = float(p1[1]) # estimated daily parameter
+    
+        
+        '''
+        Step 3 - Generate daily time series with estimated statistcs (Section 2.b.2 in text)
+        Step 4 - Add in seasonal time series component (Section 2.b.2 in text)
+        '''
+    
+        H_scale = np.sort(np.array(adj_2H) * Hi / Site_Stats['HsigB'].iloc[0])
+        O_scale = np.sort(np.array(adj_18O) * Oi / Site_Stats['OsigB'].iloc[0])
+
+        copula_stats = np.matrix([[0, 0, 0], [np.std(tsP), Hi, Oi],
+                                    [Site_Stats['PHpB'].iloc[0], 
+                                     Site_Stats['POpB'].iloc[0],
+                                     Site_Stats['HOpB'].iloc[0]]])    
+
+        new_ts = conditional_copula_ts(daily_P['Total P'], copula_stats, H_scale, O_scale)
+        y = np.array([np.array(xi) for xi in new_ts])   
+
+        # Add back in n-day sine function here:
+        tsH_daily = y[:,0] + sine_func(daily_P['FracYear'], params1[0], params1[1], params1[2])    
+        tsO_daily = y[:,1] + sine_func(daily_P['FracYear'], params2[0], params2[1], params2[2])
+    
+        daily_P['d2H'] = tsH_daily
+        daily_P['d18O'] = tsO_daily 
+
+        print('Done.')
+    
+    return daily_P
+    
+
+''' Read .xlsx precipitation and isotope data '''
+def read_p_data(sitename, precip_data_loc, precip_filter, iso_data_loc, out_dir_loc):
 
     # 30min Precipitation Data
     df_P30 = pd.read_excel(precip_data_loc,index=False)
@@ -89,13 +288,13 @@ def read_p_data(precip_data_loc, precip_filter, iso_data_loc):
     daily_P = pd.DataFrame({'Total P': precip_daily,'FracYear': frac_year})
     daily_P['Total P'].replace(0, np.nan, inplace=True)
     
-    print("Successfully read precipitation amount data...")
+    print("Successfully read "+ sitename+ " precipitation amount data...")
 
     # Biweekly Stable Water Isotope Data
     df_iso = pd.read_excel(iso_data_loc, index=False)
     df_iso = changeTimes_change_ISOdata(df_iso)
 
-    # Create Biweekly Precipitation Amount Timeseries to Correspond to Recorded Isotope Values
+    ## Create Biweekly Precipitation Amount Timeseries to Correspond to Recorded Isotope Values
     df_iso['setDate'] = pd.to_datetime(df_iso['setDate'])  
     df_iso['collectDate'] = pd.to_datetime(df_iso['collectDate']) 
 
@@ -113,7 +312,12 @@ def read_p_data(precip_data_loc, precip_filter, iso_data_loc):
     df_iso['Total P'] = P14
     del P14, i, subset
     
-    print("Successfully read precipitation isotope data...")
+    print("Successfully read "+ sitename+ " precipitation isotope data...")
     
+    
+    ''' Statistical Downscaling Method (Finkenbiner et al. 2021) '''
+    return downscaling_to_daily(sitename, daily_P, df_iso, out_dir_loc)
+    
+
 if __name__ == '__main__':
-    read_p_data(precip_data_loc, precip_filter, iso_data_loc)
+    read_p_data(sitename, precip_data_loc, precip_filter, iso_data_loc, out_dir_loc)
